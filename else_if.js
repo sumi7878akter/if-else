f8fcbd2718100cb58a780ecc5fde1c3d04a7bb35
sumi7878akter <jannatulislamsumi0@gmail.com>
2024-03-30 12:17:33 +0600

    first commit

---
+++ else_if.js
@@ -0,0 +1,17 @@
+
+var time = 18;
+
+if (time >= 1 && time <= 11) {
+  console.log("Good morning");   
+}
+else if(time >= 11 && time <= 15){
+    console.lo("Good After Noon");  
+}
+ 
+else if(time >= 15 && time <= 18){
+    console.log("Good Evening");  
+}
+ 
+else {
+  console.log("Good Night"); 
+}
---
+++ if_else.js
@@ -0,0 +1,8 @@
+var time = 13;
+
+if (time > 1 && time < 11){
+    console.log("Good morning");
+}
+else{
+    console.log("Good Night")
+}
